af970836f08867d9ebccf20a58ec1d0134bfee5d
Md. Mitul Hossain <143118872+mitulhossen@users.noreply.github.com>
2023-11-26 01:07:08 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('component.home')
+    @include('component.skills')
+    @include('component.about')
+    @include('component.project')
+    @include('component.contact')
+
+@endsection 
